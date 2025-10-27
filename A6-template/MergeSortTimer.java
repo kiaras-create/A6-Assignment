bf3d299e7577d60609f2749333fe6b8800cee512
kiaras-create <ksunilsingh@smith.edu>
2025-10-27 15:50:00 -0400

    MergeSortTimer class added

---
+++ A6-template/MergeSortTimer.java
@@ -0,0 +1,80 @@
+import java.util.ArrayDeque;
+
+public class MergeSortTimer {
+
+  public static CardPile sort(CardPile unsorted) {
+    
+    ArrayDeque<CardPile> queue = new ArrayDeque<CardPile>();
+
+    while(!unsorted.isEmpty()){
+    CardPile single = new CardPile();
+    single.addLast(unsorted.removeFirst());
+    queue.addLast(single);
+    }
+
+   
+    while (queue.size()>1){
+    CardPile pile1 = queue.removeFirst();
+    CardPile pile2 = queue.removeFirst();
+    CardPile newPile = new CardPile();
+        
+
+      while((!pile2.isEmpty() && !pile1.isEmpty())){
+        Card card1 = pile1.peekFirst();
+        Card card2 = pile2.peekFirst();
+        if (card1.compareTo(card2) > 0){
+          //merge(card1, card2)
+          newPile.add(pile2.removeFirst());
+          }
+            
+        else if (card1.compareTo(card2)< 0){
+          //merge(card2,card1)
+          newPile.add(pile1.removeFirst());
+            }
+        else if(card1.compareTo(card2) == 0){
+          //merge(card1,card2)
+          newPile.add(pile1.removeFirst());
+
+        }
+    
+        }
+
+    if (!pile1.isEmpty()){
+      while (!pile1.isEmpty()){
+        newPile.add(pile1.removeFirst());
+      }
+               
+    }
+    if (!pile2.isEmpty()){
+      while (!pile2.isEmpty()){
+        newPile.add(pile2.removeFirst());
+      }
+
+
+    }
+
+
+
+    }
+    return queue.remove();
+  }
+
+  /** Starts the program running */
+  public static void main(String args[]) {
+    
+    if (args.length<1) {
+      System.err.println("Please specify how many cards to sort!");
+    } else {
+      Card[] deck = Card.newDeck(true);
+      CardPile cards = new CardPile();
+      
+      for (int i = 0; i<Integer.parseInt(args[0]); i++ ) {
+        cards.add(deck[(int)(52*Math.random())]);
+      }
+
+      sort(cards);
+      
+    }
+  }
+}
+
