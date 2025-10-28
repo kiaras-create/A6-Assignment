aabf042ac57e9a08f030c4a4d31a20bcec3331cd
Victoria Reyes <victoriareyes@Victorias-MacBook-Air-644.local>
2025-10-27 20:48:02 -0400

    hi

--- A6-template/README.md
+++ A6-template/README.md
@@ -13,4 +13,8 @@ Any references used besides JavaDoc and course materials:
 
 What did you notice about the differences in runtime across algorithms as you changed the number of cards you were sorting? If you had to split them into "slower" algorithms vs "faster" algorithms, which would you put in each category?
 
+Victoria Reyes Borges - For the smaller piles, the algorithms seemed to go finished quickly but when there were bigger piles, they got slower. Selection adn insertion sort were slower (O(n^2)), while Quicksort and merge sort were faster (O(n log n)).
+
 Please reflect on your experience with this assignment. What was most challenging? What was most interesting?
+
+Victoria Reyes Borges - I enjoyed this aissgnment, mostlye because I like the concepts of the sorting algorithms and the different ways we can use them. I found challeging visually the algortihms mentally or without any physcial objects, so I had to use some of my personal things to figure out the logic! I found interesting how, in a way, we can "make" storage in coding but when we perform these sorting algortihms that storage is just space "in real life", so definitely tying that back what I was doing with physically visualizing my algorithm into code was interesting!
--- A6-template/SelectionSort.java
+++ A6-template/SelectionSort.java
@@ -1,5 +1,10 @@
 public class SelectionSort {
   
+  /**
+   * @param unsorted
+   * @param record
+   * @return sorted
+   */
   public static CardPile sort(CardPile unsorted, SortRecorder record) {
     
     // register the starting configuration with the recorder
@@ -18,6 +23,34 @@ public class SelectionSort {
     //        record.add(unsorted); // the unsorted pile
     // ***********************************************************
 
+    while(!unsorted.isEmpty()){ //while the unsorted list is not empty
+      Card smallest = unsorted.peek(); // take a look a the first card in the unsorted pile
+      CardPile temp = new CardPile(); // make a temporary list to hold the cards
+
+
+      while(!unsorted.isEmpty()){ //while the unsorted list is not empty
+        Card current = unsorted.peek(); // take a look at the current card we are on 
+
+        if(current.compareTo(smallest) < 0){ // if this current card we are on is less than the smallest card aka the beginning card
+          temp.add(smallest); // add the smallest (which is the card with bigger value) to the temporary list
+          smallest = current; // now the current card we were looking at is the smallest value in our list, so we'll compare the next ones to it
+        } else { // if this current card we are on is NOT less than the smallest card aka the beginning card
+          temp.add(current); // move the current card into the temporary pile
+        }
+      }
+
+      while(!temp.isEmpty()){ // while the temporary pile is not empty
+        unsorted.add(temp.pop()); //take the first card of the temporary pile and add it to the unsorted list so we can compare it to the smallest
+      }
+
+      sorted.add(smallest); //add the smallest card into the sorted pile
+
+    }
+
+    record.next();        // tell it this is a new step
+    record.add(sorted);   // the sorted pile
+    record.add(unsorted); // the unsorted pile
+    
     // return the sorted result here
     return sorted;
   }
