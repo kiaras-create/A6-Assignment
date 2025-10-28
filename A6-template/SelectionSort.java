2d0eaace9fa527a6748f6f8c3c3eec43779ec471
Victoria Reyes <victoriareyes@Victorias-MacBook-Air-644.local>
2025-10-27 21:29:10 -0400

    hi

--- A6-template/SelectionSort.java
+++ A6-template/SelectionSort.java
@@ -1,3 +1,5 @@
+import java.util.Collections;
+
 public class SelectionSort {
   
   /**
@@ -39,19 +41,56 @@ public class SelectionSort {
         }
       }
 
+
+      sorted.add(smallest); //add the smallest card into the sorted pile
+
       while(!temp.isEmpty()){ // while the temporary pile is not empty
         unsorted.add(temp.pop()); //take the first card of the temporary pile and add it to the unsorted list so we can compare it to the smallest
       }
 
-      sorted.add(smallest); //add the smallest card into the sorted pile
+      
 
     }
 
     record.next();        // tell it this is a new step
     record.add(sorted);   // the sorted pile
     record.add(unsorted); // the unsorted pile
+
+    
     
     // return the sorted result here
     return sorted;
   }
+
+  public static void main(String args[]) {
+
+    // set up a class to record and display the sorting results
+    SortRecorder recorder = new SortRecorder();
+
+    // set up the deck of cards
+    Card.loadImages(recorder);
+    CardPile cards = new CardPile(Card.newDeck(true), 2, 2);
+
+    // for debugging purposes, uncomment this to
+    // work with a smaller number of cards:
+    cards = cards.split(cards.get(39));
+
+    // mix up the cards
+    Collections.shuffle(cards);
+   
+    // if you want to sort in array form, use:
+    // Card[] card_arr = cards.toArray(new Card[0]);
+
+    // in your program, this would be a call to a real sorting algorithm
+    cards = SelectionSort.sort(cards, recorder);
+
+    // We can print out the (un)sorted result:
+    System.out.println(cards);
+
+    // make window appear showing the record
+    recorder.display("Card Sort Demo: SelectionSort");
+  }
+}
+
+ 
 }
