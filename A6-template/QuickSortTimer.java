de53a009a0bef3ff6b5c440654068599eb40cd32
cyang-05 <chiashiiyang@gmail.com>
2025-10-29 00:01:01 -0400

    quicksort comments

--- A6-template/QuickSortTimer.java
+++ A6-template/QuickSortTimer.java
@@ -1,81 +1,97 @@
 import java.util.ListIterator;
 
+
 public class QuickSortTimer {
-  public static CardPile sort(CardPile unsorted) {
 
-    // stop condition
-    // if list has 0 or 1 elements, return list as it's already sorted
-    if (unsorted.size() == 0 || unsorted.size() == 1) {
-      return unsorted;
-    }
-      
-    // Here are the two partitions you will be creating
-    CardPile smaller = new CardPile();
-    CardPile bigger = new CardPile();
-
+ /**
+  * sorts unsorted card pile into algorithm
+  * @param unsorted card pile
+  * @return a sorted pile
+  */
+ public static CardPile sort(CardPile unsorted) {
   
-    // pivot will be the first element of the list
-    Card pivot = unsorted.getFirst();
-    // will keep track of current position in list
-    ListIterator<Card> iter = unsorted.listIterator();
-    // move iterator to go past the pivot
-    Card curr = iter.next();
-
-    // iterates over card pile until no more cards to iterate
-    while (iter.hasNext()) {
-      curr = iter.next();
-      if (curr.compareTo(pivot) > 0) {
-        // debugging purposes
-          System.out.println("current card is bigger! moving it to the bigger cardpile");
-          bigger.add(curr);
-          iter.remove();
-      } else if (curr.compareTo(pivot) < 0 ) {
-          System.out.println("current card is smaller! moving it to smaller cardpile");
-          smaller.add(curr);
-          iter.remove();
-      } else {
-          System.out.println("current card is of equal value! moving it to the bigger cardpile, it'll still be sorted for final result");
-          bigger.add(curr);
-          iter.remove();
-      }
-    } 
-
-    // This will hold the assembled result
-    CardPile result = new CardPile();
-
-    // recursive call to sort smaller and bigger piles
-    sort(smaller);
-    sort(bigger);
-
-    // add smaller pile to assembled result first 
-    // then pivot
-    // then bigger pile 
-    result.addAll(smaller);
-    result.add(pivot);
-    result.addAll(bigger);
-
-    
-    // return the sorted result here
-    return result;
+  // stop condition
+  // if list has 0 or 1 elements, return list as it's already sorted
+  if (unsorted.size() == 0 || unsorted.size() == 1) {
+    return unsorted;
   }
+  
+  // Here are the two partitions you will be creating
+  CardPile smaller = new CardPile();
+  CardPile bigger = new CardPile();
 
-  /** Starts the program running */
-  public static void main(String args[]) {
-    
-    if (args.length<1) {
-      System.err.println("Please specify how many cards to sort!");
+
+  // pivot will be the first element of the list
+  Card pivot = unsorted.getFirst();
+  // will keep track of current position in list
+  ListIterator<Card> iter = unsorted.listIterator();
+  // move iterator to go past the pivot
+  Card curr = iter.next();
+
+
+  // iterates over card pile until no more cards to iterate
+  while (iter.hasNext()) {
+    curr = iter.next();
+    if (curr.compareTo(pivot) > 0) {
+      // debugging purposes
+        // System.out.println("current card is bigger! moving it to the bigger cardpile");
+        bigger.add(curr);
+        iter.remove();
+    } else if (curr.compareTo(pivot) < 0 ) {
+        // System.out.println("current card is smaller! moving it to smaller cardpile");
+        smaller.add(curr);
+        iter.remove();
     } else {
-      Card[] deck = Card.newDeck(true);
-      CardPile cards = new CardPile();
-      
-      for (int i = 0; i<Integer.parseInt(args[0]); i++ ) {
-        cards.add(deck[(int)(52*Math.random())]);
-      }
-
-      sort(cards);
-      
+        // System.out.println("current card is of equal value! moving it to the bigger cardpile, it'll still be sorted for final result");
+        bigger.add(curr);
+        iter.remove();
     }
   }
+
+
+  // This will hold the assembled result
+  CardPile result = new CardPile();
+
+
+  // recursive call to sort smaller and bigger piles
+  sort(smaller);
+  sort(bigger);
+
+
+  // add smaller pile to assembled result first
+  // then pivot
+  // then bigger pile
+  result.addAll(smaller);
+  result.add(pivot);
+  result.addAll(bigger);
+
+
+
+  // return the sorted result here
+  return result;
+ }
+
+
+ /** Starts the program running */
+ public static void main(String args[]) {
+  
+   if (args.length<1) {
+     System.err.println("Please specify how many cards to sort!");
+   } else {
+     Card[] deck = Card.newDeck(true);
+     CardPile cards = new CardPile();
+    
+     for (int i = 0; i<Integer.parseInt(args[0]); i++ ) {
+       cards.add(deck[(int)(52*Math.random())]);
+     }
+
+
+     sort(cards);
+    
+   }
+ }
+
 
 }
+
 
--- A6-template/Quicksort.java
+++ A6-template/Quicksort.java
@@ -1,114 +1,134 @@
 import java.util.Collections;
 import java.util.ListIterator;
 
+
 public class Quicksort {
-  
-  public static CardPile sort(CardPile unsorted, SortRecorder record) {
 
-    record.add(unsorted);
+ /**
+  * sorts unsorted card pile into algorithm
+  * @param unsorted card pile
+  * @param record records movements of cards
+  * @return sorted card pile
+  */
+ public static CardPile sort(CardPile unsorted, SortRecorder record) {
 
-    // stop condition
-    // if list has 0 or 1 elements, return list as it's already sorted
-    if (unsorted.size() == 0 || unsorted.size() == 1) {
-      return unsorted;
+   record.add(unsorted);
+
+
+  // stop condition
+  // if list has 0 or 1 elements, return list as it's already sorted
+  if (unsorted.size() == 0 || unsorted.size() == 1) {
+    return unsorted;
+  }
+
+  // Here are the two partitions you will be creating
+  CardPile smaller = new CardPile();
+  CardPile bigger = new CardPile();
+
+
+  record.add(smaller);
+  record.add(bigger);
+
+
+  // pivot will be the first element of the list
+  Card pivot = unsorted.getFirst();
+  // will keep track of current position in list
+  ListIterator<Card> iter = unsorted.listIterator();
+  // move iterator to go past the pivot
+  Card curr = iter.next();
+
+
+  // iterates over card pile until no more cards to iterate
+  while (iter.hasNext()) {
+    curr = iter.next();
+    if (curr.compareTo(pivot) >= 0) {
+      // debugging purposes
+      // System.out.println("current card is bigger! moving it to the bigger cardpile");
+      bigger.add(curr);
+      iter.remove();
+    } else if (curr.compareTo(pivot) < 0 ) {
+        // System.out.println("current card is smaller! moving it to smaller cardpile");
+        smaller.add(curr);
+        iter.remove();
+    } else {
+      // System.out.println("current card is of equal value! moving it to the bigger cardpile, it'll still be sorted for final result");
+      bigger.add(curr);
+      iter.remove();
     }
-    
-    // Here are the two partitions you will be creating
-    CardPile smaller = new CardPile();
-    CardPile bigger = new CardPile();
+  }
 
-    record.add(smaller);
-    record.add(bigger);
 
+  // register the partitions with the recorder
+  record.add(smaller);
+  record.add(pivot);
+  record.add(bigger);
+  record.next();
+
+  // This will hold the assembled result
+  CardPile result = new CardPile();
+
+  // recursive call to sort smaller and bigger piles
+  sort(smaller, record);
+  record.add(smaller);
+
+
+  sort(bigger, record);
+  record.add(bigger);
+
+  // add smaller pile to assembled result first
+  // then pivot
+  // then bigger pile
+  result.addAll(smaller);
+  result.add(pivot);
+  result.addAll(bigger);
+
+
+  // record the sorted result
+  record.add(result);
+  record.next();
+
+  // return the sorted result here
+  return result;
   
-    // pivot will be the first element of the list
-    Card pivot = unsorted.getFirst();
-    // will keep track of current position in list
-    ListIterator<Card> iter = unsorted.listIterator();
-    // move iterator to go past the pivot
-    Card curr = iter.next();
-
-    // iterates over card pile until no more cards to iterate
-    while (iter.hasNext()) {
-      curr = iter.next();
-      if (curr.compareTo(pivot) >= 0) {
-        // debugging purposes
-        // System.out.println("current card is bigger! moving it to the bigger cardpile");
-        bigger.add(curr);
-        iter.remove();
-      } else if (curr.compareTo(pivot) < 0 ) {
-          // System.out.println("current card is smaller! moving it to smaller cardpile");
-          smaller.add(curr);
-          iter.remove();
-      } else {
-        // System.out.println("current card is of equal value! moving it to the bigger cardpile, it'll still be sorted for final result");
-        bigger.add(curr);
-        iter.remove();
-      }
-    } 
+ }
 
 
-    // register the partitions with the recorder
-    record.add(smaller);
-    record.add(pivot);
-    record.add(bigger);
-    record.next();
+   public static void main(String args[]) {
 
 
-    // This will hold the assembled result
-    CardPile result = new CardPile();
+   // set up a class to record and display the sorting results
+   SortRecorder recorder = new SortRecorder();
 
 
-    // recursive call to sort smaller and bigger piles
-    sort(smaller, record);
-    record.add(smaller);
-
-    sort(bigger, record);
-    record.add(bigger);
-    
-    // add smaller pile to assembled result first 
-    // then pivot
-    // then bigger pile 
-    result.addAll(smaller);
-    result.add(pivot);
-    result.addAll(bigger);
+   // set up the deck of cards
+   Card.loadImages(recorder);
+   CardPile cards = new CardPile(Card.newDeck(true), 2, 2);
 
 
-    // record the sorted result
-    record.add(result);
-    record.next();
-    
-    // return the sorted result here
-    return result;
-  }
+   // for debugging purposes, uncomment this to
+   // work with a smaller number of cards:
+   //cards = cards.split(cards.get(39));
 
-    public static void main(String args[]) {
 
-    // set up a class to record and display the sorting results
-    SortRecorder recorder = new SortRecorder();
+   // mix up the cards
+   Collections.shuffle(cards);
+ 
+   // if you want to sort in array form, use:
+   // Card[] card_arr = cards.toArray(new Card[0]);
 
-    // set up the deck of cards
-    Card.loadImages(recorder);
-    CardPile cards = new CardPile(Card.newDeck(true), 2, 2);
 
-    // for debugging purposes, uncomment this to
-    // work with a smaller number of cards:
-    //cards = cards.split(cards.get(39));
+   // in your program, this would be a call to a real sorting algorithm
+   cards = MergeSort.sort(cards, recorder);
 
-    // mix up the cards
-    Collections.shuffle(cards);
-   
-    // if you want to sort in array form, use:
-    // Card[] card_arr = cards.toArray(new Card[0]);
 
-    // in your program, this would be a call to a real sorting algorithm
-    cards = MergeSort.sort(cards, recorder);
+   // We can print out the (un)sorted result:
+   System.out.println(cards);
 
-    // We can print out the (un)sorted result:
-    System.out.println(cards);
 
-    // make window appear showing the record
-    recorder.display("Card Sort Demo: Quick Sort");
-  }
+   // make window appear showing the record
+   recorder.display("Card Sort Demo: Quick Sort");
+ }
+
 
 }
+
